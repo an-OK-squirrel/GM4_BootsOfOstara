5df68f75e2f96baab0ad2140442e7b0404635b7b
an-OK-squirrel <an-OK-squirrel@users.noreply.github.com>
2016-07-13 14:08:29 -0500

    init

---
+++ src/main/java/co/gm4/GM4_BootsOfOstara/BootsOfOstara.java
@@ -0,0 +1,17 @@
+package co.gm4.GM4_BootsOfOstara;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BootsOfOstara extends JavaPlugin{
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
